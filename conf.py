82a7b90ad2ce2d5e6c258bfcecfce2cd8c9cff37
gheize@gmail.com <gheize@gmail.com>
2015-03-13 17:08:40 -0300

    Changes Licences conf.py

--- conf.py
+++ conf.py
@@ -593,7 +593,7 @@ RSS_LINKS_APPEND_QUERY = False
 # LICENSE = ""
 # I recommend using the Creative Commons' wizard:
 # http://creativecommons.org/choose/
- LICENSE = """
+LICENSE = """
 <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/2.5/ar/">
 <img alt="Creative Commons License BY-NC-SA"
 style="border-width:0; margin-bottom:12px;"
